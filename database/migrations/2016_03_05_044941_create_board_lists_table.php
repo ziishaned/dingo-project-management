f5cb452979966cd40f32f825a8f396c0c6be0eab
Zeeshan Ahmed <ziishaned@gmail.com>
2016-03-05 10:08:06 +0500

    php artisan migrate

--- database/migrations/2016_03_05_044941_create_board_lists_table.php
+++ database/migrations/2016_03_05_044941_create_board_lists_table.php
@@ -14,6 +14,11 @@ class CreateBoardListsTable extends Migration
     {
         Schema::create('board_lists', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('board_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('board_id')->references('id')->on('board');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('list_name')->unique();
             $table->timestamps();
         });
     }
--- database/migrations/2016_03_05_045031_create_board_cards_table.php
+++ database/migrations/2016_03_05_045031_create_board_cards_table.php
@@ -14,6 +14,16 @@ class CreateBoardCardsTable extends Migration
     {
         Schema::create('board_cards', function (Blueprint $table) {
             $table->increments('id');
+            
+            $table->integer('board_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('list_id')->unsigned();
+            
+            $table->foreign('board_id')->references('id')->on('board');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('list_id')->references('id')->on('board_lists');
+            
+            $table->string('card_title')->unique();
             $table->timestamps();
         });
     }

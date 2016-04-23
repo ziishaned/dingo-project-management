9223bbb05c75025fac2c2436d1369e38e4cc7252
Zeeshan Ahmed <ziishaned@gmail.com>
2016-04-23 15:46:07 +0500

    Add new animation (spinkit)

--- public/js/ajax-loading.js
+++ public/js/ajax-loading.js
@@ -7,16 +7,22 @@ $(document).ready(function() {
         var that = this;
         $.ajaxSetup({
           beforeSend: function() {
-            if ($("#loadingbar").length === 0) {
-              $("body").append("<div id='loadingbar'></div>")
-              $("#loadingbar").addClass("waiting").append($("<dt/><dd/>"));
-              $("#loadingbar").width((50 + Math.random() * 30) + "%");
-            }
+            $(".overlay").fadeIn('slow');
+            $(".spinner").show();
+            // if ($("#loadingbar").length === 0) {
+            //   $("body").append("<div id='loadingbar'></div>")
+            //   $("#loadingbar").addClass("waiting").append($("<dt/><dd/>"));
+            //   $("#loadingbar").width((50 + Math.random() * 30) + "%");
+            // }
           },
           complete : function() {
-            $("#loadingbar").width("101%").delay(200).fadeOut(400, function() {
-                $(this).remove();
-            });         
+            setTimeout(function() {
+              $(".overlay").fadeOut('slow');
+              $(".spinner").hide();
+            }, 700);
+            // $("#loadingbar").width("101%").delay(200).fadeOut(400, function() {
+            //     $(this).remove();
+            // });         
           }
         });         
       }

5f29d0214d77eacdcab8163576718d818c4f5f9a
Zeeshan Ahmed <ziishaned@gmail.com>
2016-04-29 05:52:29 +0500

    Added is_starred attribute

--- database/migrations/2016_04_19_212551_create_board_table.php
+++ database/migrations/2016_04_19_212551_create_board_table.php
@@ -18,6 +18,7 @@ class CreateBoardTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('boardTitle');
+            $table->boolean('is_starred');
             $table->string('boardPrivacyType');
             $table->timestamps();
         });

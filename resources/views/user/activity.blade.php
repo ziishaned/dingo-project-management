4ca5e850792d05082879878be9c6f7f499c326ba
Zeeshan Ahmed <ziishaned@gmail.com>
2016-04-30 10:19:25 +0500

    Add new activity design

--- resources/views/user/activity.blade.php
+++ resources/views/user/activity.blade.php
@@ -16,9 +16,45 @@
                 </ul>
 	    	</div>
 	    </div>
-        <div class="row">
-            <div class="col-lg-12">
-                
+        <div class="row" style="margin-top: 15px;">
+            <div class="col-lg-8 col-lg-offset-2">
+                <ul>
+                    <li>
+                        <div class="media">
+                            <div class="pull-left" style="    display: block; border: 1px solid #DCDCDC; border-radius: 4px; box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.17); padding: 3px;">
+                                <img class="media-object" src="{{ asset('img/user_1.jpg') }}" style="width: 35px;">
+                            </div>
+                            <div class="media-body" style="padding-left: 15px; padding-top: 1px;">
+                                <p style="margin-bottom: 1px;"><b>Zeeshan Ahmed</b> completed a task on Laravel in Today Board Name Welcome Board</p>
+                                <p style="margin-bottom: 1px;"><span class="glyphicon glyphicon-time" aria-hidden="true"></span> Apr, 29 2016 at 9:55 pm</p>
+                            </div>
+                        </div>
+                    </li>
+                    <hr>
+                    <li>
+                        <div class="media">
+                            <div class="pull-left" style="    display: block; border: 1px solid #DCDCDC; border-radius: 4px; box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.17); padding: 3px;">
+                                <img class="media-object" src="{{ asset('img/user_1.jpg') }}" style="width: 35px;">
+                            </div>
+                            <div class="media-body" style="padding-left: 15px; padding-top: 1px;">
+                                <p style="margin-bottom: 1px;"><b>Zeeshan Ahmed</b> completed a task on Laravel in Today Board Name Welcome Board</p>
+                                <p style="margin-bottom: 1px;"><span class="glyphicon glyphicon-time" aria-hidden="true"></span> Apr, 29 2016 at 9:55 pm</p>
+                            </div>
+                        </div>
+                    </li>
+                    <hr>
+                    <li>
+                        <div class="media">
+                            <div class="pull-left" style="    display: block; border: 1px solid #DCDCDC; border-radius: 4px; box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.17); padding: 3px;">
+                                <img class="media-object" src="{{ asset('img/user_1.jpg') }}" style="width: 35px;">
+                            </div>
+                            <div class="media-body" style="padding-left: 15px; padding-top: 1px;">
+                                <p style="margin-bottom: 1px;"><b>Zeeshan Ahmed</b> completed a task on Laravel in Today Board Name Welcome Board</p>
+                                <p style="margin-bottom: 1px;"><span class="glyphicon glyphicon-time" aria-hidden="true"></span> Apr, 29 2016 at 9:55 pm</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>

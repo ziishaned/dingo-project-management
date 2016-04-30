5c1028272c088a09a347d6f336ccb4f199481205
Zeeshan Ahmed <ziishaned@gmail.com>
2016-04-30 11:08:51 +0500

    UserActivity Model Modified

--- app/Models/UserActivity.php
+++ app/Models/UserActivity.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class UserActivity extends Model
 {
-    //
+    protected $table = "user_activity";
+
+ 	protected $fillable = [
+        'user_id', 'changed_in', 'activity_in_id', 'activity_description', 'created_at', 'updated_at',
+    ];   
 }

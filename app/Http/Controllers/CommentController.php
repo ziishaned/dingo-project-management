c854c733116ccb3ca6853c1da3ad80b0bd7f0217
Zeeshan Ahmed <ziishaned@gmail.com>
2016-06-09 15:18:55 +0500

    Model functions are moved from CommentController to Comment Model

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -14,6 +14,12 @@ use \App\Models\Comment;
 
 class CommentController extends Controller
 {
+    protected $comment;
+
+    public function __construct(Comment $comment)
+    {
+      $this->comment = $comment;
+    }
     /**
      * Crates or insert a new comment in the database.
      * @param  Request $request have the comment as input for this function
@@ -21,21 +27,8 @@ class CommentController extends Controller
      */
     public function saveComment(Request $request)
     {
-        $comment = $request->get("comment");
-        $cardId = $request->get("cardId");
-        $userId  = Auth::id();
-
-        $commentDetail = Comment::create([
-            'card_id' => $cardId,
-            'user_id' => $userId,
-            'comment_description' => $comment,  
-        ]);
-
-        return DB::table('comment')
-          ->select('comment.*', 'users.name')
-          ->join('users','users.id','=','comment.user_id')
-          ->where('comment.id','=',$commentDetail["id"])
-          ->get();
+        $commentDetail = $this->comment->saveComment($request, Auth::id());
+        return $this->comment->getComment($commentDetail["id"]);
     }
     
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,4 +11,21 @@ class Comment extends Model
     protected $fillable = [
         'card_id', 'user_id', 'comment_description', 'created_at', 'updated_at',
     ];
+
+    public function saveComment($input, $user_id)
+    {
+    	return Comment::create([
+            'card_id' => $input->get("cardId"),
+            'user_id' => $user_id,
+            'comment_description' => $input->get("comment"),  
+        ]);
+    }
+
+    public function getComment($commentId)
+    {
+    	return Comment::select('comment.*', 'users.name')
+          ->join('users','users.id','=','comment.user_id')
+          ->where('comment.id','=', $commentId)
+          ->get();
+    }
 }
